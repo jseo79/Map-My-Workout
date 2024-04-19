4dac9c6d56bb1fb09d44d65b7d15801058c40c12
jseo79 <joseph79seo@gmail.com>
2024-04-18 22:41:35 -0500

    minor change

---
+++ workout.js
@@ -0,0 +1,69 @@
+class Workout {
+	date = new Date();
+	id = (Date.now() + '').slice(-10);
+
+	constructor(coords, distance, duration) {
+		this.coords = coords;
+		this.distance = distance;
+		this.duration = duration;
+	}
+
+	_setDescription() {
+		const months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		];
+
+		this.description = `${this.type[0].toUpperCase()}${this.type.slice(
+			1
+		)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+	}
+}
+
+class Running extends Workout {
+	type = 'running';
+
+	constructor(coords, distance, duration, cadence) {
+		super(coords, distance, duration);
+		this.cadence = cadence;
+		this.calcPace();
+		this._setDescription();
+	}
+
+	calcPace() {
+		this.pace = this.duration / this.distance;
+		return this.pace;
+	}
+}
+
+class Biking extends Workout {
+	type = 'biking';
+
+	constructor(coords, distance, duration, elevationGain) {
+		super(coords, distance, duration);
+		this.elevationGain = elevationGain;
+		this.calcSpeed();
+		this._setDescription();
+	}
+
+	calcSpeed() {
+		this.speed = (this.distance / this.duration) * 60;
+		return this.speed;
+	}
+}
+
+module.exports = {
+	Workout,
+	Running,
+	Biking,
+};
