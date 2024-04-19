a411fdcf6946f0c34bd99a518f1dba67079a18d5
jseo79 <joseph79seo@gmail.com>
2024-04-18 20:43:48 -0500

    merge

--- public/main.js
+++ public/main.js
@@ -10,73 +10,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 const resetButton = document.querySelector('.button_reset');
 const closeButtons = document.querySelectorAll('.close_buttons');
 
-let map, mapEvent;
-
-class Workout {
-	date = new Date();
-	id = (Date.now() + '').slice(-10);
-
-	constructor(coords, distance, duration) {
-		this.coords = coords;
-		this.distance = distance;
-		this.duration = duration;
-	}
-
-	_setDescription() {
-		const months = [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December',
-		];
-
-		this.description = `${this.type[0].toUpperCase()}${this.type.slice(
-			1
-		)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
-	}
-}
-
-class Running extends Workout {
-	type = 'running';
-
-	constructor(coords, distance, duration, cadence) {
-		super(coords, distance, duration);
-		this.cadence = cadence;
-		this.calcPace();
-		this._setDescription();
-	}
-
-	calcPace() {
-		this.pace = this.duration / this.distance;
-		return this.pace;
-	}
-}
-
-class Biking extends Workout {
-	type = 'biking';
-
-	constructor(coords, distance, duration, elevationGain) {
-		super(coords, distance, duration);
-		this.elevationGain = elevationGain;
-		this.calcSpeed();
-		this._setDescription();
-	}
-
-	calcSpeed() {
-		this.speed = (this.distance / this.duration) * 60;
-		return this.speed;
-	}
-}
-
-class App {
+class ClientApp {
 	#map;
 	#mapEvent;
 	#workouts = [];
@@ -336,4 +270,4 @@ class App {
 	}
 }
 
-const app = new App();
+const clientApp = new ClientApp();
